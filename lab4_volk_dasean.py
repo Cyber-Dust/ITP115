c031010e330c71e952cb02c4065f044dc746df3f
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-09-25 17:10:42 -0700

    Create lab4_volk_dasean.py

---
+++ lab4_volk_dasean.py
@@ -0,0 +1,24 @@
+# Dasean Volk, dvolk@usc.edu
+# Section: Boba
+# Loops
+# Lab 4
+
+# create variables
+numSpaces = 18
+numSymbols = 1
+
+# loop
+for row in range(10):
+    print(" " * numSpaces + "^ " * numSymbols)
+    numSymbols = numSymbols + 2
+    numSpaces = numSpaces - 2
+
+# challenge 1
+print("\nChallenge #1")
+for symbol in range(1, 20, 2):
+    print(" " * (19 - symbol) + "^ " * symbol)
+
+# challenge 2
+print("\nChallenge #2")
+for symbol in range(19, 0, -2):
+    print(" " * (19 - symbol) + "b " * symbol)
