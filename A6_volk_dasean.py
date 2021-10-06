32792f207462f72b3fdf0362a53c5cef4e8d23b1
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-10-05 18:48:57 -0700

    Create A6_volk_dasean.py

---
+++ A6_volk_dasean.py
@@ -0,0 +1,30 @@
+# Dasean Volk, dvolk@usc.edu
+# ITP 115, Fall 2021
+# Section: Boba
+# Assignment 6
+# Description:
+
+# import the random module
+import random
+
+# create 2 lists that 1. contains words and 2. contains the hints for those words
+
+words = ['love', 'movie', 'car', 'video game']
+hints = ['a warm and fuzzy feeling', 'motion pictures', 'an engine and four wheels', 'people play these digitally']
+
+# get random word with the corresponding hint and put them into a variable
+
+wordVar = randrange(words)
+
+# create a jumbled word
+
+# print the jumbled word to the user
+print("The jumbled word is ", )
+
+# have the user guess the word until they get it correct, using a loop
+# count the number of guesses
+# if the guess is incorrect, ask if they want a hint
+# give them the hint
+
+# if correct, print the number of guesses
+print("You got it! \n It took you ", guesses, "guesses")
