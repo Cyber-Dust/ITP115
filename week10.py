e4b2b83f5c0a459011e81ff3eeacb68aaf36a313
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-10-25 17:32:08 -0700

    Create week10.py

---
+++ week10.py
@@ -0,0 +1,8 @@
+# Dasean Volk, dvolk@usc.edu
+# FALL 2021, ITP115
+# Week X Notes
+
+#-------------------------------------------FUNCTIONS PT2-------------------------------------------------#
+
+# Functions
+# Immutable Variables = not affeced by changes in the function
