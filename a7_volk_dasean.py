1a31a03d2adea1372c4f6e520376d7e40e6ea008
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-10-27 18:26:39 -0700

    Create a7_volk_dasean.py

---
+++ a7_volk_dasean.py
@@ -0,0 +1,21 @@
+# Dasean Volk, dvolk@usc.edu
+# ITP 115, Fall 2021
+# Section: Boba
+# Assignment 7 
+# Description:
+#-----------------------------------------------NAME: TITLE---------------------------------------------------------#
+
+import random
+
+# define displayMenu() function
+# Parameters (input): None
+# o Return value (output): None
+# o Displays the game rules to the user:
+
+displayMenu():
+  print("Let's play Rock Paper Scissors."
+        "The rules of the game are:"
+        "Rock smashes Scissors"
+        "Scissors cut Paper"
+        "Paper covers Rock"
+        "If both the choices are the same, it's a tie")
