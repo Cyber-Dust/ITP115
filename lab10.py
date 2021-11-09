8dacc81d33e89135c677a51efdec027d20a237d2
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-11-08 17:11:37 -0800

    Create lab10.py

---
+++ lab10.py
@@ -0,0 +1,29 @@
+# Dasean Volk, dvolk@usc.edu
+# Fall 2021, ITP 115
+# Section: Boba
+# LAB 10
+
+# -------DICTIONAIRIES-------- #
+
+# JSON over dictionaries
+
+# Stores information in Key Pairs
+# Like a dictionary where each entry is a key pair
+
+# SYNTAX
+# Define dictionary w/ {}
+
+# ex.) 
+# my_dictionary = {key:value1, key2:value2,key3:value3}
+
+# You can access items by key (dictionary) rather than a position (list)
+
+# -- METHODS --
+
+# len(dict)
+# dict.get(key, [default])
+# dict.pop(key, [default])
+# dict.keys()
+# dict.values()
+# dict.items()
+# del dict[key]
