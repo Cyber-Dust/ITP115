1e3339453aac8983fd4b0903ca86d8756827b7a0
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-11-16 00:29:33 -0800

    Update a10_volk_dasean.py

--- a10_volk_dasean.py
+++ a10_volk_dasean.py
@@ -1,5 +1,32 @@
 # Dasean Volk, dvolk@usc.edu
-# Fall 2021, ITP 115
+# ITP 115, Fall 2021
 # Section: Boba
 # Assignment 10
+# Description:
+
+# import music library helper file
+import MusicLibraryHelper
+
+# import random module
+import random
+
+# Define the displayMenu() function.
+# o Parameter: None
+# o Return value: None
+# o Print out the menu options to the user:
+def display_menu():
+    print("Manage Your Music Library"
+            "\na) Display library"
+            "\nb) Display artists"
+            "\nc) Add an artist/album"
+            "\nd) Delete an album"
+            "\ne) Delete an artist"
+            "\nf) Generate a random playlist"
+            "\ng) Exit")
+
+
+# Define the displayLibrary(dictionary) function.
+# o Parameter: a dictionary representing the music library
+# o Return value: None
+# def display_library(dictionary):
 
