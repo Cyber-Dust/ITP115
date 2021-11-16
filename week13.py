d7d5be5bca68bcaf6449bce1fe5e7a8fc13131f4
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-11-15 17:52:50 -0800

    Update week13.py

--- week13.py
+++ week13.py
@@ -1,1 +1,37 @@
+# Week 13
+# Boba Class
 
+# OOP = object-oriented programming
+# define a class (blueprint or template) - only define once
+# crete objects from the class
+# classes are made up of attributes (var) and behaviors (methods)
+# methods are functions defined inside of a class
+
+# class: Movie
+# required attributes: title, year
+# attributes: genre, length/runtime, cast, rating
+class Movie:
+    def __init__(self, title, year):
+        # create attributes
+        self.title = title
+        self.year = year
+        self.genre = ""
+        self.length = 0 # in min
+        self.rating = "NR"
+        
+    def __str__(self):
+        # must return a string
+        info = self.title + " (" + str(self.year) + ")"
+        return info
+      
+def main():
+  print("It's Movie Time!")
+  # create movie objects
+  
+  print("Dasean's favorite movie")
+  dasean = Movie("Drive", 2011)
+  dasean.genre = "Crime Drama"
+  dasean.length = 140 + "min"
+  print(dasean)
+  
+main()
