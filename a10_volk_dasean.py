62b9289e412e4fee51a6ff6a32b742bce6318b28
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-11-18 21:56:41 -0800

    Update a10_volk_dasean.py

--- a10_volk_dasean.py
+++ a10_volk_dasean.py
@@ -4,6 +4,8 @@
 # Assignment 10
 # Description:
 
+# ------------------------------------WELCOME TO THE MUSIC LIBRARY GENERATOR----------------------------------------- #
+
 # import music library helper file
 import MusicLibraryHelper
 
@@ -62,17 +64,44 @@ def add_album(dictionary="musicLibrary.dat"):
     new_album = input("Enter Album: ").title()
     # if artists is in dictionary, add album to the existing list of albums
     if new_artist in artists:
-        artists.append(new_album)
+        artists[new_artist].append(new_album)
     #     if new_album in artists:
+    #     if new_album in artists: # how do I NOT add something?
     # elif new_artist not in artists:
+    elif new_artist not in artists:
+        artists[new_artist] = [new_artist].append(new_album)
+        # along with the new value
     print(artists)
     # if the album exists, then do not add it
     # if artist is not in the dictionary. add a new key (artists), add new value(list containing the album)
 
+# Define the deleteAlbum(dictionary) function.
+# o Parameter: a dictionary representing the music library
+# o Return value: a boolean value - True if an album was successfully deleted, or
+# False if the album was not successfully deleted
+
+
+def delete_album(dictionary):
+    dictionary = "musicLibrary.dat"
+    artist = input("Enter artist: ").title()
+    album = input("Enter album: ").title()
+    # if artist in dictionary and album in dictionary:
+       # delete the corresponding inputs from dictionary
+    # return true or false
+
+# Define the deleteArtist(dictionary) function.
+# o Parameter: a dictionary representing the music library
+# o Return value: a boolean value - True if an artist was successfully deleted, or False
+# if the artist was not successfully deleted
+
+
+def delete_artist(dictionary):
+    artist = input("Enter artist to delete: ").title()
+
 
 def generate_random_playlist(dictionary="musicLibrary.dat"):
     library = MusicLibraryHelper.loadLibrary(dictionary)
-    print("Here is your random playlist: \n")
+    print("Here is your random playlist:")
     for key in library:
         print(random.choice(library[key]), "by", key)
 
@@ -92,8 +121,8 @@ def main():
         elif new_game == "f":
             generate_random_playlist()
         # elif new_game == "g":
-    # new_library = input("Enter music library filename: ") 
-    # new_library = MusicLibraryHelper.saveLibrary(new_library) # how do you do this? 
+    # new_library = input("Enter music library filename: ")
+    # MusicLibraryHelper.saveLibrary(new_library, musicLibDct) # how do you do this?
     # # the dictionary representing the music library
     # print("Saved music library to ", new_library)
 
@@ -102,3 +131,4 @@ display_library()
 display_artists()
 generate_random_playlist()
 add_album()
+
