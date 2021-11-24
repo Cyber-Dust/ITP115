a569ee636b7f5043ffaad9bbade95c6e7654c568
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-11-23 23:46:37 -0800

    Create final.py

---
+++ final.py
@@ -0,0 +1,9 @@
+# Dasean Volk, dvolk@usc.edu
+# Fall 2021, ITP115
+# Section: Boba
+# Final Project
+
+# --------------------------------------------------------WELCOME TO THE FINAL------------------------------------------------------------- #
+
+# use the "nationalparks.csv"
+# .csv files = comma serperated values
