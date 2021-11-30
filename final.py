c31edb82688999dc8db7be1e556b7aa76d064e95
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-11-29 18:27:10 -0800

    Update final.py

--- final.py
+++ final.py
@@ -1,15 +1,29 @@
+
 # Dasean Volk, dvolk@usc.edu
 # Fall 2021, ITP115
 # Section: Boba
 # Final Project
 
-# --------------------------------------------------------WELCOME TO THE FINAL------------------------------------------------------------- #
+# ----------------------------------------------WELCOME TO THE FINAL-------------------------------------------------- #
 
 # use the "nationalparks.csv"
 # .csv files = comma serperated values
 
 # finish this by december 10
 
+# Parameter: fileName is the name of the CSV file to read and it has a default
+# value of "national_parks.csv"
+# • Return value: a list of dictionary objects where the keys are the strings from
+# the header row and the values are the information from the rest of the CSV file
 
-def create_list_of_parks():
-  
+def create_list_of_parks(file_name):
+    list = []
+    fin = open(file_name, "r")
+    header = fin.readline()
+    print(header)
+
+def main():
+    parks = "national_parks.csv"
+    create_list_of_parks(parks)
+
+main()
