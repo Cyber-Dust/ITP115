5429e28d408efd12eee493bbf3ec869b5041f04b
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2021-12-01 23:50:52 -0800

    Update final.py

--- final.py
+++ final.py
@@ -1,29 +1,47 @@
-
 # Dasean Volk, dvolk@usc.edu
-# Fall 2021, ITP115
+# ITP 115, Fall 2021
 # Section: Boba
 # Final Project
+# tasks.py
+# Description:
 
-# ----------------------------------------------WELCOME TO THE FINAL-------------------------------------------------- #
+# import interface
+import interface
 
-# use the "nationalparks.csv"
-# .csv files = comma serperated values
-
-# finish this by december 10
-
-# Parameter: fileName is the name of the CSV file to read and it has a default
+# Define the createListOfParks() function.
+# • Parameter: fileName is the name of the CSV file to read and it has a default
 # value of "national_parks.csv"
 # • Return value: a list of dictionary objects where the keys are the strings from
-# the header row and the values are the information from the rest of the CSV file
+# the header row and the values are the information from the rest of the CSV
+# file
+
+
 
 def create_list_of_parks(file_name):
     list = []
     fin = open(file_name, "r")
     header = fin.readline()
+    header = header.strip()
+    for line in header:
+        line = line.strip()
+        parks_list = line.split(",")
+
     print(header)
+
+# def get_date(data_str):
+
+def display_menu(menu_dict):
+    interface.get_menu_dict()
+
 
 def main():
     parks = "national_parks.csv"
+    display_menu(menu_dict=interface)
     create_list_of_parks(parks)
 
 main()
+
+
+
+
+# def get_date(data_str, ):
