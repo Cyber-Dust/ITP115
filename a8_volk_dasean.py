452c3025c5f1dadfa7eeb8020f7d33db99ecf8da
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2022-04-24 23:47:39 -0700

    Update a8_volk_dasean.py

--- a8_volk_dasean.py
+++ a8_volk_dasean.py
@@ -1,6 +1,4 @@
 # Dasean Volk, dvolk@usc.edu
-# Fall 2021, ITP 115
-# Section: Boba
 # Assignment 8
 
 # -----------------------------------------TIC TAC TOE--------------------------------------------------- #
